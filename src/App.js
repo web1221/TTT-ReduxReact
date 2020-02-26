64f43b445116789840e50707d65d4fd82b149ead
Student <example@example.com>
2020-02-26 15:26:57 -0800

    finish tic tac toe react tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Game from './components/Game';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/ticTacToe-reducer'
 import './App.css';
+
+const store = createStore(reducer)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Provider store={store}>
+        <Game />
+      </Provider>
     </div>
   );
 }
